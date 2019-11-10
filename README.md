7c30b4948d09280b7534b97fc0764d7defe27c57
bebaek <bebaek@gmail.com>
2019-11-10 15:26:56 -0600

    DEV: add setup-env.sh

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # briefer
 Send personalized briefing email daily
+
+## Development
+
+### Setting up environment
+
+Prerequisites: git, bash
+
+1. Download repo.
+
+    git clone https://github.com/bebaek/briefer.git
+
+2. Run setup script.
+
+    bash ci/setup-env.sh
---
+++ ci/setup-env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Set up Python env.
+# For Ubuntu/Debian only yet.
+
+echo Start: set up Python environment.
+
+devenv=briefer
+pkgs=()
+
+# Install edm
+# FIXME: implement
+
+# Create edm env
+edm env remove -y $devenv
+edm env create --version 3 $devenv
+
+# Install Python packages
+if [ ${#pkgs[@]} -ne 0 ]; then
+    edm install -e $devenv req
+fi
+
+echo End: set up Python environment.
